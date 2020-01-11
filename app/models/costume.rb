a9d6b633e0bb867e98613ce23b4d66b3362fa274
Vesna Vucinic <vesna.vucinic68@gmail.com>
2020-01-11 00:24:43 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,8 +1,6 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
 class Costume < ActiveRecord::Base
-<<<<<<< HEAD
+
 end
-=======
-end
->>>>>>> 9eea389c61f81b3ef6b35d21c9cf3ff2b30a3fe6
+
