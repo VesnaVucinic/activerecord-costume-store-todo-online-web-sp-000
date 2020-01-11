6af6a921a09a1cd04d6a8e11c53517e745dae4b9
Vesna Vucinic <vesna.vucinic68@gmail.com>
2020-01-11 00:24:28 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,9 +1,5 @@
 # Create your CostumeStore class here
-<<<<<<< HEAD
-class CostumeStore < ActiveRecord::Base
-end
-=======
 
 class CostumeStore < ActiveRecord::Base
 end
->>>>>>> 9eea389c61f81b3ef6b35d21c9cf3ff2b30a3fe6
+
